b7844c5523de50c2d13d519007dce307793628ef
Volodymyr Trotsyshyn <volodymyr.trotsyshyn@tictrac.com>
2021-02-07 11:49:02 +0200

    P2P support

--- frpc/run.sh
+++ frpc/run.sh
@@ -7,6 +7,7 @@ if bashio::fs.file_exists $configPath; then
   rm $configPath
 fi
 echo "[common]" >> $configPath
+echo "bind_udp_port = 7001" >> $configPath
 echo "server_addr = $(bashio::config 'server_addr')" >> $configPath
 echo "server_port = $(bashio::config 'server_port')" >> $configPath
 if bashio::var.has_value "$(bashio::config 'token')"; then
@@ -28,16 +29,40 @@ for id in $(bashio::config "tunnels|keys"); do
   if bashio::var.has_value "$(dig +short ${local_ip})"; then
     echo "local_ip = $(dig +short ${local_ip})" >> $configPath
   else
-    echo "local_ip = ${local_ip}" >> $configPath
+    if bashio::var.has_value "${local_ip}"; then
+      echo "local_ip = ${local_ip}" >> $configPath
+    fi
   fi
   local_port=$(bashio::config "tunnels[${id}].local_port")
-  echo "local_port = $local_port" >> $configPath
+  if bashio::var.has_value $local_port; then
+    echo "local_port = ${local_port}" >> $configPath
+  fi
   subdomain=$(bashio::config "tunnels[${id}].subdomain")
   if bashio::var.has_value $subdomain; then
     echo "subdomain = ${subdomain}" >> $configPath
   fi
   echo "use_encryption = true" >> $configPath
   echo "use_compression = true" >> $configPath
+  sk=$(bashio::config "tunnels[${id}].sk")
+  if bashio::var.has_value $sk; then
+    echo "sk = ${sk}" >> $configPath
+  fi
+  role=$(bashio::config "tunnels[${id}].role")
+  if bashio::var.has_value $role; then
+    echo "role = ${role}" >> $configPath
+  fi
+  server_name=$(bashio::config "tunnels[${id}].server_name")
+  if bashio::var.has_value $server_name; then
+    echo "server_name = ${server_name}" >> $configPath
+  fi
+  bind_addr=$(bashio::config "tunnels[${id}].bind_addr")
+  if bashio::var.has_value $bind_addr; then
+    echo "bind_addr = ${bind_addr}" >> $configPath
+  fi
+  bind_port=$(bashio::config "tunnels[${id}].bind_port")
+  if bashio::var.has_value $bind_port; then
+    echo "bind_port = ${bind_port}" >> $configPath
+  fi
 done
 
 bashio::log.info "Config file ${configPath} generated:"
